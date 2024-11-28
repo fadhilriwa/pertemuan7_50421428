30fd5cbfba33dea223ecda6c179d52722ae4b563
fadhilriwa <faadhil.riwa@gmail.com>
2024-11-28 16:42:48 +0700

    new code

--- main.java
+++ main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(string[]args){
         system.out.println(x:"Hello World");
+
+        String[] books = {"Java Programming", "Database Design", "Web Development"};
+
+        System.out.println("Available Books:"); // Perbaiki 'system' menjadi 'System'
+
+        for (String book : books) {
+            System.out.println("- " + book); // Perbaiki 'system' menjadi 'System'      
     }
+}
 }
